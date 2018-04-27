4114b400077447005e524977945fb0b4711ecadd
Pierre-Yves Monarque <pierre-yves.monarque@autodesk.com>
2018-04-27 09:59:13 -0700

    some tests

--- tests/exo.test.js
+++ tests/exo.test.js
@@ -7,24 +7,51 @@ const height = 1080;
 
 beforeAll(async () => {
     browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         args: [`--window-size=${width},${height}`]
       });
     page = await browser.newPage();
+
+    //Start jeopardy app from react exo branch exoPart3 before for those tests to run properly
     await page.goto("http://localhost:3000/");
 });
 
-describe("Test the geopardy application", () => {
+describe("Test the jeopardy application", () => {
     
     test("make sure page title is React App", async () => {
       const pageTitle = await page.title();
       expect(pageTitle).toBe("React App");
     }, 16000);
 
-    test("verify qeustion appears", async () => {
+    test("Does question: appears", async () => {
         const question = await page.$("#question");
         expect(question).not.toBe(null);
     }, 16000);
+
+    test("Does a question appears", async () => {
+        let questionText = await page.$eval("#currentQuestion", (el) => {
+            return el.innerHTML
+        });
+        expect(questionText).not.toBe("");
+    }, 16000);
+
+    /*
+    test("Make sure the score those update", async () => {
+
+    }, 16000);
+    */
+
+    /*
+    test("Make sure the form gets empty after submit", async () => {
+
+    }, 16000);
+    */
+
+    /*
+    test("Make sure a new question appears after submit", async () => {
+
+    }, 16000);
+    */
 
 });
     
